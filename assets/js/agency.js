8c6ad9ee4911b4039a7e602ba48ee54681e5035c
andrewjose <andrewjose@users.noreply.github.com>
2018-11-03 19:31:34 -0700

    related target test

--- assets/js/agency.js
+++ assets/js/agency.js
@@ -46,7 +46,7 @@
   $('.portfolio-modal').on('hidden.bs.modal', function(e) {
     $(".navbar").removeClass("d-none");
     // reloads the iframe so that if a video is playing, stops it
-    //$(".portfolio-modal iframe").attr("src", $(".portfolio-modal iframe").attr("src"));
+    $(e.relatedTarget iframe).attr("src", $(e.relatedTarget iframe).attr("src"));
     // destroys iframe so that switching modals reloads the youtube embed
     //$(".portfolio-modal iframe").empty();
   })
